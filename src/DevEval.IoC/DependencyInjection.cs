2abcbacfdd8d6c983701604f66b482b03aa780b2
André N. Darcie <andrendarcie@gmail.com>
2025-02-04 19:55:03 -0300

    feat(ioc): add dependency injection configuration
    - Added `DependencyInjection.cs` to centralize dependency registration for the application.
    - Updated `DevEval.IoC.csproj` to include the new configuration file.
    
    This introduces the IoC (Inversion of Control) setup to streamline dependency management across the project.

---
+++ src/DevEval.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DevEval.Domain.Repositories;
+using DevEval.ORM.Repositories;
+using DevEval.Application.Sales.Services;
+using DevEval.Common.Services;
+
+namespace DevEval.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ISaleRepository, SaleRepository>();
+
+            services.AddSingleton<IPasswordService, PasswordService>();
+            services.AddScoped<ISaleEventPublisher, SaleEventPublisher>();
+
+            return services;
+        }
+    }
+}
