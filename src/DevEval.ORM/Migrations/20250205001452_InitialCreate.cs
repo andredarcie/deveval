1c27fa475a22fd6e7c8ad986c57ad27ee3c48c58
André N. Darcie <andrendarcie@gmail.com>
2025-02-04 21:15:59 -0300

    feat: add initial database migration
    - Created the initial migration to define the database schema.
    - Added tables and columns as per the current data model.
    - Includes migration snapshot for tracking future changes.

---
+++ src/DevEval.ORM/Migrations/20250205001452_InitialCreate.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevEval.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cart",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<DateTime>(type: "TIMESTAMPTZ", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cart", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false),
+                    category = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    image = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sale",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    sale_number = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    sale_date = table.Column<DateTime>(type: "TIMESTAMPTZ", nullable: false),
+                    is_cancelled = table.Column<bool>(type: "boolean", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    branch_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    branch_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sale", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    username = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    street = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    number = table.Column<int>(type: "integer", nullable: true),
+                    zip_code = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    latitude = table.Column<double>(type: "double precision", nullable: true),
+                    longitude = table.Column<double>(type: "double precision", nullable: true),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cart_product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    product_id = table.Column<int>(type: "integer", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    cart_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cart_product", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_cart_product_cart_cart_id",
+                        column: x => x.cart_id,
+                        principalTable: "cart",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sale_item",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    discount = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    is_cancelled = table.Column<bool>(type: "boolean", nullable: false),
+                    SaleId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sale_item", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_sale_item_sale_SaleId",
+                        column: x => x.SaleId,
+                        principalTable: "sale",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cart_product_cart_id",
+                table: "cart_product",
+                column: "cart_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sale_item_SaleId",
+                table: "sale_item",
+                column: "SaleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cart_product");
+
+            migrationBuilder.DropTable(
+                name: "product");
+
+            migrationBuilder.DropTable(
+                name: "sale_item");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "cart");
+
+            migrationBuilder.DropTable(
+                name: "sale");
+        }
+    }
+}
